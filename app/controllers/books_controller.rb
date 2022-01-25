a43d2aec9be4a5d891da0f2ed94f7bcfec5acea8
r32main <r32main@gmail.com>
2022-01-25 15:11:24 +0200

    add order

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @pagy, @books = pagy Book.order(created_at: :desc)
+    if params[:order].in? %w[asc desc]
+      @pagy, @books = pagy Book.order(id: params[:order])
+    else
+      @pagy, @books = pagy Book.order(id: 'desc')
+    end
+    @pagy, @books = pagy Book.order(name: params[:order_name]) if params[:order_name].in? %w[asc desc]
+    @pagy, @books = pagy Book.order(price: params[:order_price]) if params[:order_price].in? %w[asc desc]
+    @pagy, @books = pagy Book.order(availability: params[:order_av]) if params[:order_av].in? %w[asc desc]
   end
 end
 
